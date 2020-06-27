8df72e1ef8f3d0ccee3931460e4e5bf20971995e
erickkaiquesilva <erickkay272@gmail.com>
2020-06-26 23:24:29 -0300

    Create module

---
+++ DSEasy/Example/DSEasy/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DSEasy
+//
+//  Created by erickkaiquesilva on 06/26/2020.
+//  Copyright (c) 2020 erickkaiquesilva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ DSEasy/Example/Pods/Target Support Files/DSEasy/DSEasy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DSEasy : NSObject
+@end
+@implementation PodsDummy_DSEasy
+@end
